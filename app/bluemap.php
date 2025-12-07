1d47208eb8c7a19d3dc23d11bd489250e682b4e2
Mark Jerwin <136870415+jrwnnnn@users.noreply.github.com>
2025-12-07 10:30:04 +0800

    Embed BlueMap iframe and update layout
    Replaces the server offline placeholder with an embedded BlueMap iframe. Updates the page structure for a more streamlined layout and removes unused loading and offline UI elements.

--- app/bluemap.php
+++ app/bluemap.php
@@ -21,24 +21,12 @@ $zoom = isset($_GET['zoom']) ? floatval($_GET['zoom']) : 1250;
     <link href="public/css/output.css" rel="stylesheet">
     <title>Block1A - BlueMap</title>
   </head>
-  <body class="flex flex-col min-h-screen">
-    <section class="bg-[#1A212B] bg-cover bg-center bg-no-repeat flex flex-col h-screen">
-      <?php require 'views/partials/navigation.php'; ?>
-      <div class="flex flex-grow flex-col justify-center items-center bg-[#2D3748]">
-        <div class="flex flex-col items-center justify-center pb-10">
-          <img src="public/assets/images/ui/panda-roll-loading.gif" alt="Server Closed" class="w-30 h-30">
-          <p class="text-2xl font-bold text-white md:text-4xl">Server offline...</p>
-          <p class="text-white">No map will be shown until the server opens.</p>
-        </div>
+  <body class="flex flex-col min-h-[100dvh] justify-between">
+    <?php require 'views/partials/navigation.php'; ?>
+    <main class="flex bg-[#1A212B] flex-grow">
+      <div class="flex-grow">
+        <iframe id="iframe" src="https://bluemap-proxy.onrender.com/bluemap/#<?= $world . ':' . $x . ':0:' . $z . ':' . $zoom ?>:0:0:0:0:perspective" class="w-full h-full"></iframe>
       </div>
-      <!-- <div class="flex flex-grow flex-col justify-center items-center bg-[#2D3748]">
-        <div id="loading" class="flex flex-col items-center justify-center bg-[#2D3748] pb-10">
-          <img src="public/assets/images/ui/panda-roll-loading.gif" alt="Loading" class="w-30 h-30">
-          <p class="text-2xl font-bold text-white md:text-4xl">Loading Map...</p>
-          <p class="text-white">Hang tight while we load this.</p>
-        </div>
-        <iframe id="iframe" src="https://bluemap-proxy.onrender.com/bluemap/#<?= $world . ':' . $x . ':0:' . $z . ':' . $zoom ?>:0:0:0:0:perspective" class="hidden w-full h-full" onload="const t = Math.floor(Math.random() * 5 + 1) * 1000; setTimeout(() => { document.getElementById('loading').style.display = 'none'; document.getElementById('iframe').classList.remove('hidden'); }, t);"></iframe>
-      </div> -->
-    </section>
+    </main>
   </body>
 </html>
